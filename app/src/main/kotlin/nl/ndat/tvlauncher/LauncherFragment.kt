b5c1417d10c0701e3abb2ce69b425330f5ff00f6
Niels van Velzen <git@ndat.nl>
2021-04-26 16:19:25 +0200

    Small design improvements

--- app/src/main/kotlin/nl/ndat/tvlauncher/LauncherFragment.kt
+++ app/src/main/kotlin/nl/ndat/tvlauncher/LauncherFragment.kt
@@ -1,11 +1,14 @@
 package nl.ndat.tvlauncher
 
+import android.animation.ValueAnimator
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import nl.ndat.tvlauncher.databinding.FragmentLauncherBinding
 import nl.ndat.tvlauncher.databinding.ViewCardAppBinding
@@ -26,6 +29,15 @@ class LauncherFragment : Fragment() {
 	}
 
 	private fun addEventListeners() {
+		binding.button.setOnFocusChangeListener { _, hasFocus ->
+			val color = ContextCompat.getColor(requireContext(), if (hasFocus) R.color.lb_tv_white else R.color.lb_grey)
+			val animator = ValueAnimator.ofArgb(binding.button.imageTintList!!.defaultColor, color)
+			animator.addUpdateListener {
+				binding.button.imageTintList = ColorStateList.valueOf(it.animatedValue as Int)
+			}
+			animator.duration = 200
+			animator.start()
+		}
 		binding.button.setOnClickListener {
 			startActivity(Intent(Settings.ACTION_SETTINGS))
 		}
