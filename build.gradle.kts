a84881e5c88b815c236497181570dabe60e214cc
Niels van Velzen <git@ndat.nl>
2021-06-23 16:02:00 +0200

    Update Android build tools and Kotlin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
+        classpath("com.android.tools.build:gradle:4.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
     }
 }
