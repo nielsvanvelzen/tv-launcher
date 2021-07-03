f45558661d32b694f4f0476a408f2744cf4d4d4a
Niels van Velzen <git@ndat.nl>
2021-07-03 20:44:39 +0200

    Update apps on activity resume

--- app/src/main/kotlin/nl/ndat/tvlauncher/LauncherActivity.kt
+++ app/src/main/kotlin/nl/ndat/tvlauncher/LauncherActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import androidx.lifecycle.lifecycleScope
+import nl.ndat.tvlauncher.data.repository.TileRepository
 import nl.ndat.tvlauncher.databinding.ActivityLauncherBinding
 import nl.ndat.tvlauncher.ui.LauncherFragment
 import nl.ndat.tvlauncher.util.DefaultLauncherHelper
@@ -11,6 +13,7 @@ import org.koin.android.ext.android.inject
 
 class LauncherActivity : FragmentActivity() {
 	private val defaultLauncherHelper: DefaultLauncherHelper by inject()
+	private val tileRepository: TileRepository by inject()
 	private lateinit var binding: ActivityLauncherBinding
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +27,12 @@ class LauncherActivity : FragmentActivity() {
 		}
 
 		validateDefaultLauncher()
+		lifecycleScope.launchWhenResumed {
+			with(tileRepository) {
+				refreshAllApplications()
+				refreshAllInputs()
+			}
+		}
 	}
 
 	private fun validateDefaultLauncher() {
