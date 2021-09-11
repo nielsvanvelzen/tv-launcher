ce8be0bc4986fe52c0c9dde4ccb4b8193853cdd7
Niels van Velzen <git@ndat.nl>
2021-09-11 11:24:17 +0200

    Rewrite LayoutManager for smoother scrolling

--- app/src/main/kotlin/nl/ndat/tvlauncher/ui/recyclerview/AutoGridLayoutManager.kt
+++ app/src/main/kotlin/nl/ndat/tvlauncher/ui/recyclerview/AutoGridLayoutManager.kt
@@ -1,9 +1,7 @@
 package nl.ndat.tvlauncher.ui.recyclerview
 
 import android.content.Context
-import android.view.FocusFinder
 import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Recycler
@@ -11,7 +9,7 @@ import kotlin.math.max
 
 class AutoGridLayoutManager(
 	context: Context,
-	var columnWidth: Int,
+	private var columnWidth: Int,
 ) : GridLayoutManager(context, 1) {
 	private var lastColumnWidth = 0
 	private var lastWidth = 0
@@ -35,20 +33,49 @@ class AutoGridLayoutManager(
 		super.onLayoutChildren(recycler, state)
 	}
 
+	override fun calculateExtraLayoutSpace(state: RecyclerView.State, extraLayoutSpace: IntArray) {
+		val firstChild = getChildAt(0) ?: return super.calculateExtraLayoutSpace(state, extraLayoutSpace)
+
+		// Measure the size of a single item
+		// assuming all items have equals sizes
+		val singleItemSpace = if (orientation == VERTICAL) firstChild.height else firstChild.width
+
+		// Request 1 additional item to enable scrolling
+		extraLayoutSpace[0] = singleItemSpace // Start spacing (top/left)
+		extraLayoutSpace[1] = singleItemSpace // End spacing (bottom/right)
+	}
+
+	private fun View.getRecyclerview(): RecyclerView {
+		var view = parent
+		while (view !is RecyclerView) view = view.parent
+		return view
+	}
+
 	override fun onInterceptFocusSearch(focused: View, direction: Int): View? {
-		// Get next focus item if any
-		val view = FocusFinder.getInstance()
-			.findNextFocus(focused.parent as ViewGroup, focused, direction)
-			?.let(::findContainingItemView)
-			?: return null
+		val recyclerView = focused.getRecyclerview()
+		val currentPosition = recyclerView.getChildLayoutPosition(focused)
 
-		val currentPosition = getPosition(focused)
-		val newPosition = getPosition(view)
+		val nextPosition = when (direction) {
+			// UP - DOWN
+			View.FOCUS_UP -> currentPosition - spanCount
+			View.FOCUS_DOWN -> currentPosition + spanCount
+			// NEXT - PREVIOUS
+			View.FOCUS_FORWARD -> currentPosition + 1
+			View.FOCUS_BACKWARD -> currentPosition - 1
+			// LEFT - RIGHT
+			View.FOCUS_LEFT -> if (currentPosition.floorDiv(spanCount) == (currentPosition - 1).floorDiv(spanCount)) currentPosition - 1 else currentPosition
+			View.FOCUS_RIGHT -> if (currentPosition.floorDiv(spanCount) == (currentPosition + 1).floorDiv(spanCount)) currentPosition + 1 else currentPosition
 
-		val sameColumn = currentPosition % spanCount == newPosition % spanCount
-		val sameRow = currentPosition.floorDiv(spanCount) == newPosition.floorDiv(spanCount)
+			// Unknown direction
+			else -> currentPosition
+		}
 
-		return if (sameColumn || sameRow) view
-		else focused
+		// Scroll to new position if it exists
+		if (nextPosition != currentPosition && nextPosition in 0..itemCount) {
+			val nextView = findViewByPosition(nextPosition)
+			return nextView ?: focused
+		}
+
+		return super.onInterceptFocusSearch(focused, direction)
 	}
 }
