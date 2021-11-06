a3916f2842ac3662e2750c4118e3be62ee530c2e
Niels van Velzen <git@ndat.nl>
2021-11-06 14:37:20 +0100

    Use plugin management for Kotlin/Android plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,13 +9,19 @@ pluginManagement {
 	}
 
 	plugins {
+		id("org.jetbrains.kotlin.android") version "1.5.31"
 		id("com.google.devtools.ksp") version "1.5.31-1.0.0"
+		id("com.android.application") version "7.0.3"
 	}
 }
 
-dependencyResolutionManagement.repositories {
-	mavenCentral()
-	google()
+dependencyResolutionManagement {
+	repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+	repositories {
+		mavenCentral()
+		google()
+	}
 }
 
 include(":app")
