cb7b4a2d7b0da06bffe120cd0355bd4871010be2
Niels van Velzen <git@ndat.nl>
2023-03-26 12:15:21 +0200

    Remove LocalOverscrollConfiguration provider

--- app/src/main/kotlin/nl/ndat/tvlauncher/ui/AppBase.kt
+++ app/src/main/kotlin/nl/ndat/tvlauncher/ui/AppBase.kt
@@ -1,19 +1,15 @@
 package nl.ndat.tvlauncher.ui
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import nl.ndat.tvlauncher.ui.page.LauncherPage
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun AppBase() {
 	MaterialTheme(
@@ -27,12 +23,8 @@ fun AppBase() {
 			large = RoundedCornerShape(8.dp)
 		),
 	) {
-		CompositionLocalProvider(
-			LocalOverscrollConfiguration provides null,
-		) {
-			Surface {
-				LauncherPage()
-			}
+		Surface {
+			LauncherPage()
 		}
 	}
 }
