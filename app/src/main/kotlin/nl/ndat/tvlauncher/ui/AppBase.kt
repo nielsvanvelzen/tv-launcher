94ed6c2774191d1b2d92f5c2c0b58838b588c42b
Niels van Velzen <git@ndat.nl>
2024-05-26 16:50:14 +0200

    Remove custom indication

--- app/src/main/kotlin/nl/ndat/tvlauncher/ui/AppBase.kt
+++ app/src/main/kotlin/nl/ndat/tvlauncher/ui/AppBase.kt
@@ -1,16 +1,13 @@
 package nl.ndat.tvlauncher.ui
 
-import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Shapes
 import androidx.tv.material3.Surface
 import androidx.tv.material3.darkColorScheme
-import nl.ndat.tvlauncher.ui.indication.FocusScaleIndication
 import nl.ndat.tvlauncher.ui.page.LauncherPage
 
 @Composable
@@ -26,12 +23,8 @@ fun AppBase() {
 			large = RoundedCornerShape(8.dp)
 		),
 	) {
-		CompositionLocalProvider(
-			LocalIndication provides FocusScaleIndication
-		) {
-			Surface {
-				LauncherPage()
-			}
+		Surface {
+			LauncherPage()
 		}
 	}
 }
